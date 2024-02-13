9d5d78bb0513089a6de2b5b878132dababb1f0f0
geraldbahati <journeytoharvard@gmail.com>
2024-02-13 09:28:08 +0300

    done with login process uaer can login

--- app/src/main/java/com/example/ecommerceapp/features/auth/presentation/registration/login/LoginEvent.kt
+++ app/src/main/java/com/example/ecommerceapp/features/auth/presentation/registration/login/LoginEvent.kt
@@ -1,4 +1,15 @@
 package com.example.ecommerceapp.features.auth.presentation.registration.login
 
-class LoginEvent {
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+sealed class LoginEvent {
+    data class OnSetNavigator(val navigator: DestinationsNavigator): LoginEvent()
+    data class OnEmailChange(val email: String): LoginEvent()
+    data class OnPasswordChange(val password: String): LoginEvent()
+
+    data object OnLogin: LoginEvent()
+    data object OnNavigateToRegister: LoginEvent()
+    data object OnNavigateToForgotPassword: LoginEvent()
+    data object OnNavigateToHome: LoginEvent()
+    data object OnPasswordVisibilityChange: LoginEvent()
 }
